727be9100fcaa150b5e8dce7177f4eb2f52ef317
Ryan Davey <ryancdavey@icloud.com>
2018-06-29 07:41:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,39 +1,34 @@
-//define functions here
-function getIt(){
-  console.log('paragraph');
-  $('p').on("click", function(){
-    alert("Hey!");
-  });
+
+
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
 }
 
-function frameIt(){
-  console.log('frame');
-  $("img").addClass("tasty");
-  $('img').on('load', function(){
-     $('img').addClass('tasty');
-  });
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
 }
 
-function pressIt(){
-  console.log('press');
-  $('input').on('keydown', function(key) {
-        if(key.which == 71){
-          alert('g was pressed');
-        }
-  });
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
 }
 
-function submitIt(){
-  console.log('submitted');
-  $("form").on("submit", function() {
-    alert('your form is going to be submitted now');
-});
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
+
 // call functions here
-  getIt();
-  frameIt();
-  pressIt();
-  submitIt();
+
 });

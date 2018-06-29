09984d3a5ed2d4a66110acf442a8195984e983c2
Ryan Davey <ryancdavey@icloud.com>
2018-06-29 07:43:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -37,28 +37,3 @@ $(document).ready(function(){
 });
 
 
-
-
-
-
-
-function pressIt() {
-  $('#typing').on('keydown', function(e) {
-    // 'g' corresponds to 71
-    if (e.which === 71) {
-      alert("You pressed 'g'!")
-    }
-  })
-}
-
-function submitIt() {
-  $('form').on('submit', function(e) {
-    alert('Your form is going to be submitted now.')
-  })
-}
-
-$(document).ready(function(){
-
-// call functions here
-
-});

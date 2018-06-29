675d66fea8c13693ef5729f389a1c4544ca2bab3
Ryan Davey <ryancdavey@icloud.com>
2018-06-29 07:41:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,3 +1,44 @@
+//define functions here
+function getIt(){
+  console.log('paragraph');
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  console.log('frame');
+  $("img").addClass("tasty");
+  $('img').on('load', function(){
+     $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  console.log('press');
+  $('input').on('keydown', function(key) {
+        if(key.which == 71){
+          alert('g was pressed');
+        }
+  });
+}
+
+function submitIt(){
+  console.log('submitted');
+  $("form").on("submit", function() {
+    alert('your form is going to be submitted now');
+});
+}
+
+$(document).ready(function(){
+// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
+});
+
+
 
 
 function getIt() {
